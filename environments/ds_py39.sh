4a8c489ad733c0269cdc1449a1001753e31ebe8d
Bo Wen <bowenwen@users.noreply.github.com>
2022-11-26 23:42:13 +0000

    add jupyter to environment

--- environments/ds_py39.sh
+++ environments/ds_py39.sh
@@ -1,3 +1,3 @@
-conda create -y -n ds_py39 -c conda-forge --override-channels python=3.9 "numpy>=1.16.1" "pandas>=1.1.0" "geopandas>=0.10.2" "psutil>=4.1" "pyarrow>=2.0" "numba>0.51.2" "pyyaml>=5.1" "requests>=2.7" pytest pytest-cov coveralls pycodestyle pytest-regressions jupyterlab matplotlib descartes pandasql scipy seaborn pyodbc sqlalchemy openpyxl xlrd xlsxwriter sympy nose scikit-learn scikit-learn-intelex autopep8 pip ipykernel
-conda env export -n ds_py39 -f environments/ds_py39.yml --no-builds --no-prefix
+conda create -y -n ds_py39 -c conda-forge --override-channels python=3.9 "numpy>=1.16.1" "pandas>=1.1.0" "geopandas>=0.10.2" "psutil>=4.1" "pyarrow>=2.0" "numba>0.51.2" "pyyaml>=5.1" "requests>=2.7" pytest pytest-cov coveralls pycodestyle pytest-regressions jupyter jupyterlab matplotlib descartes pandasql scipy seaborn pyodbc sqlalchemy openpyxl xlrd xlsxwriter sympy nose scikit-learn scikit-learn-intelex autopep8 pip ipykernel
+conda env export -n ds_py39 -f environments/ds_py39.yml --no-builds
 conda deactivate
